ba5c4f6520da4b31295011dad07957f5ab02f15d
Brad Wilson <dotnetguy@gmail.com>
2015-09-21 10:21:18 -0700

    Fixes #539: Add support for xUnit.net 2.1

--- src/CreateUnitTest.Xunit/XunitFrameworkProvider_2_1.cs
+++ src/CreateUnitTest.Xunit/XunitFrameworkProvider_2_1.cs
@@ -6,14 +6,14 @@ using Microsoft.VisualStudio.TestPlatform.TestGeneration.Model;
 namespace CreateUnitTest.Xunit
 {
     [Export(typeof(IFrameworkProvider))]
-    public class XunitFrameworkProvider_2_0 : XunitFrameworkProvider
+    public class XunitFrameworkProvider_2_1 : XunitFrameworkProvider
     {
         [ImportingConstructor]
-        public XunitFrameworkProvider_2_0(IServiceProvider serviceProvider, IConfigurationSettings configurationSettings, INaming naming, IDirectory directory)
+        public XunitFrameworkProvider_2_1(IServiceProvider serviceProvider, IConfigurationSettings configurationSettings, INaming naming, IDirectory directory)
             : base(serviceProvider, configurationSettings, naming, directory,
-                   displayName: "xUnit.net 2.0",
-                   xunitPackageVersion: "2.0.0",
-                   xunitCoreAssemblyVersionPrefix: "2.0.0.",
+                   displayName: "xUnit.net 2.1",
+                   xunitPackageVersion: "2.1.0",
+                   xunitCoreAssemblyVersionPrefix: "2.1.0.",
                    visualStudioRunnerPackageVersion: "2.1.0")
         { }
     }
--- src/IntelliTest.Xunit/XunitTestFramework_2_1.cs
+++ src/IntelliTest.Xunit/XunitTestFramework_2_1.cs
@@ -4,13 +4,13 @@ using Microsoft.Pex.Engine.ComponentModel;
 namespace IntelliTest.Xunit
 {
     [Serializable]
-    sealed class XunitTestFramework_2_0 : XunitTestFramework
+    sealed class XunitTestFramework_2_1 : XunitTestFramework
     {
-        public XunitTestFramework_2_0(IPexComponent host)
+        public XunitTestFramework_2_1(IPexComponent host)
             : base(host,
                    name: "xunit-2.0",
-                   prettyName: "xUnit.net 2.0",
-                   xunitPackageVersion: "2.0.0",
+                   prettyName: "xUnit.net 2.1",
+                   xunitPackageVersion: "2.1.0",
                    visualStudioRunnerPackageVersion: "2.1.0")
         { }
     }
