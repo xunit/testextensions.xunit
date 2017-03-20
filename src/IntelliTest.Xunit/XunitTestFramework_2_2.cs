1e07b47d6bb6b137b80b217a01f5ca17ee85c595
Jacek Blaszczynski <jacek.blaszczynski@outlook.com>
2017-03-20 09:25:22 +0100

    Add xUnit v2.2 support

---
+++ src/IntelliTest.Xunit/XunitTestFramework_2_2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Pex.Engine.ComponentModel;
+
+namespace IntelliTest.Xunit
+{
+    [Serializable]
+    sealed class XunitTestFramework_2_2 : XunitTestFramework
+    {
+        public XunitTestFramework_2_2(IPexComponent host)
+            : base(host,
+                   name: "xunit-2.2",
+                   prettyName: "xUnit.net 2.2",
+                   xunitPackageVersion: "2.2.0",
+                   visualStudioRunnerPackageVersion: "2.2.0")
+        { }
+    }
+}
