efb6bcbe092fe0411760721ada781c84ad8c5541
Abhitej Anoop John Bandi <aajohn@microsoft.com>
2019-09-04 18:36:53 -0700

    Create Unit Test extension update for Xunit - Ensured that this installs on the latest VS. - Validate Create Unit Test support with .Net Core. - Added a provider for the latest xunit Fx and Adapter versions and removed those for earlier less used versions. - Simplified project creation logic. - Using the base implementation to determine IsTestProject checks similar to Nunit. We could split this up if we need to in the future - At the moment I dont see a need.

--- src/CreateUnitTest.Xunit/XunitFrameworkProvider.cs
+++ src/CreateUnitTest.Xunit/XunitFrameworkProvider.cs
@@ -10,7 +10,6 @@ namespace CreateUnitTest.Xunit
     public abstract class XunitFrameworkProvider : FrameworkProviderBase
     {
         readonly string displayName;
-        readonly string xunitCoreAssemblyVersionPrefix;
 
         public XunitFrameworkProvider(IServiceProvider serviceProvider,
                                       IConfigurationSettings configurationSettings,
@@ -18,30 +17,16 @@ namespace CreateUnitTest.Xunit
                                       IDirectory directory,
                                       string displayName,
                                       string xunitPackageVersion,
-                                      string xunitCoreAssemblyVersionPrefix,
                                       string visualStudioRunnerPackageVersion)
             : base(new XunitSolutionManager(serviceProvider, naming, directory, xunitPackageVersion, visualStudioRunnerPackageVersion),
                    new XunitUnitTestProjectManager(serviceProvider, naming),
                    new XunitUnitTestClassManager(configurationSettings, naming))
         {
             this.displayName = displayName;
-            this.xunitCoreAssemblyVersionPrefix = xunitCoreAssemblyVersionPrefix;
         }
 
         public override string AssemblyName => "xunit.core";
 
         public override string Name => displayName;
-
-        public override bool IsTestProject(Project project)
-        {
-            if (project == null)
-                throw new ArgumentNullException("project");
-
-            var vsProject2 = project.Object as VSProject2;
-            if (vsProject2 != null && (project.Kind == ProjectTypes.CSharp || project.Kind == ProjectTypes.VisualBasic))
-                return vsProject2.References.AsEnumerable().Any(r => r.Name == AssemblyName && r.Version.StartsWith(xunitCoreAssemblyVersionPrefix));
-
-            return false;
-        }
     }
 }
